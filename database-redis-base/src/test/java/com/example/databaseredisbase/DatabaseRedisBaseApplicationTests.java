8693aaa09f7043877f4f081a8829c566653177ef
AmosMin <amos.min@helium10.com>
2024-12-26 09:50:42 +0800

    feat:redis nx

--- database-redis-base/src/test/java/com/example/databaseredisbase/DatabaseRedisBaseApplicationTests.java
+++ database-redis-base/src/test/java/com/example/databaseredisbase/DatabaseRedisBaseApplicationTests.java
@@ -6,11 +6,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.Assert;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest
 class DatabaseRedisBaseApplicationTests {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
+
+
+
+	public void setWithExpire(String key, Object value, long expire, TimeUnit unit) {
+		redisTemplate.opsForValue().set(key, value, expire, unit);
+	}
+
+	public Boolean setIfAbsentWithExpire(String key, String value, long ttl, TimeUnit unit) {
+		Boolean result = redisTemplate.opsForValue().setIfAbsent(key, value, ttl, unit);
+		return result != null ? result : false;
+	}
+
 
 	@Test
 	void contextLoads() {
@@ -19,6 +33,16 @@ class DatabaseRedisBaseApplicationTests {
 		//delete
 		redisTemplate.delete("name");
 		Assert.isNull(redisTemplate.opsForValue().get("name"), "名字不对");
+	}
+
+	@Test
+	void testNx(){
+		Boolean b = setIfAbsentWithExpire("name", "张三", 2, TimeUnit.MINUTES);
+		Assert.isTrue(b, "名字不对");
+		Boolean b2 = setIfAbsentWithExpire("name", "张三", 2, TimeUnit.MINUTES);
+		Assert.isTrue(!b2, "名字不对");
+		//delete
+		redisTemplate.delete("name");
 	}
 
 }
